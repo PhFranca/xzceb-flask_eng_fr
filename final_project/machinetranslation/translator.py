8c5365024b1542f7d00db3e4dd6e02be83e135b5
PhFranca <phlavio.franca@outlook.com>
2023-06-17 20:45:50 -0400

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -14,7 +14,7 @@ def english_to_french(english_text):
     Returns:
         str: The translated French text.
     """
-    translator = MyMemoryTranslator(source='EN', target='FR')
+    translator = MyMemoryTranslator(source='en', target='fr')
     french_text = translator.translate(english_text)
     return french_text
 
@@ -28,6 +28,6 @@ def french_to_english(french_text):
     Returns:
         str: The translated English text.
     """
-    translator = MyMemoryTranslator(source='FR', target='EN')
+    translator = MyMemoryTranslator(source='fr', target='en')
     english_text = translator.translate(french_text)
     return english_text
